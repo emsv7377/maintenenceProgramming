5e2fb416b198e9b93a475b0a909373b43a85ba05
mnordness <michaela.nordness.6056@student.uu.se>
2023-05-03 10:20:56 +0200

    Added score counter, hide cheese when eaten (currently only hiding the previous cheese)

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -7,17 +7,17 @@ import Rat from './Rat';
 function GameBoard(props) {
   const rows = [];
   const { width, height } = props;
-  console.log(props)
+  //console.log(props)
   const [position, setPosition] = useState({ x: 1, y: 1 });
-  
+  // Ratmans position
   Rat.position = position;
-  
+
+  const [hiddenCheese, setHiddenCheese] = useState({ x:1, y:1, hidden: true})  
   const [points, setPoints] = useState(0); 
 
   // Handles key presses (arrow keys)
   useEffect(() => {
     const handleKeyDown = (event) => {
-
     // Update position based on arrow key pressed
     //console.log(event.key);
     switch (event.key) {
@@ -37,97 +37,80 @@ function GameBoard(props) {
         // Ignore other keys
         break;
     }
+    // Checks if Ratman steps on a cell with cheese
+    checkRatManPosition(rows, position.x, position.y);
   };
   window.addEventListener('keydown', handleKeyDown);
   return () => {
     window.removeEventListener('keydown', handleKeyDown);
   };
-},);
+},[rows, position.x, position.y]);
 
-// Creates a game board with cells as a matrice 
- const occupiedCell = ''; 
- // const occupiedCell = 'cheese';
+// Creates a game board with cells as a matrice
+  // cellValue shows type of cell 
+ const cellValue = ''; 
   for (let y = 0; y < height; y++) {
     const cells = [];
 
     for (let x = 0; x < width; x++) {
-      cells.push({ x, y, occupiedCell });
+      cells.push({ x, y, cellValue });
     }
     rows.push(cells);
   }
  
-  // Opens up the tutorial 
+  // Navigates to up the tutorial
+  // TODO: Implement tutorial opening
   function navTutorial(){
     console.log('Tutorial not implemented')
   }
-
-  /**
-   * Searches through each element in an array and nested arrays to compare x and y values
-   * @param {*} rows Array consisting of arrays with cells 
-   * @param {*} x The coordinate x to search for 
-   * @param {*} y 
-   * @returns An array with the indices of the match, if no match [-1,-1] is returned 
-   */
-function findIndex(rows, x, y){
-  let index = [-1,-1]
-  // Since the array rows is a matrix we need to search th
-    for (let i = 0; i < rows.length; i++){
-      for(let j = 0; j < rows[i].length; j++){
-        if(rows[i][j].x === x && rows[i][j].y === y){
-          index = [i,j];
-          return index;
-        } 
-      }
-    }
-    return index;
-   }
-
+   
 /**
- * Updates the occupiedCell parameter of a chosen cell in the array rows 
+ * Updates the cellValue parameter of a chosen cell in the array rows 
  * @param {*} rows The array of cells 
- * @param {*} x The x coordinate of the chosen cell
- * @param {*} y The y coordinate of the chosen cell 
- * @param {*} value The new occupiedCell value for x and y
+ * @param {String} x The x coordinate of the chosen cell
+ * @param {String} y The y coordinate of the chosen cell 
+ * @param {String} value  the new cellValue value for x and y
  * @returns The updated rows array  
  */
- function updateOccupiedCell(rows, x, y, value){
-
+ function updateCellValue(rows, x, y, newValue){
   // Find index of the current coordinates in the array rows 
-  let index = findIndex(rows, x, y);
-  // Catch error if invalid index 
-  if(index === [-1,-1]){
-    // TODO: catch error somehow  
-    console.log('Error, did not find element')
-    return;
-  } 
-    // Update the occupiedCell value to indicate type of cell
-    const row = rows[index[0]][index[1]];
-    row.occupiedCell = value;
+  if(rows[y]){
+    let cell = rows[y][x];
+    if(cell){
+      cell.cellValue = newValue;
+      }
     return rows;
  }
+}
 
  /**
   * Checks what type of cell RatMan stands on 
   * @param {*} rows The game board
-  * @param {*} x The x coordinate for Ratmans position
-  * @param {*} y The y coordinate for Ratmans position
+  * @param {String} x The x coordinate for Ratmans position
+  * @param {String} y The y coordinate for Ratmans position
+  * @returns the cell value 
   */
  function checkRatManPosition(rows, x, y){
-  // Finds index in the game board for the position 
-  let index = findIndex(rows, x, y)
-  const row = rows[index[0]][index[1]]; 
-  // Value of the current cell
-  const value = row.occupiedCell;
-  console.log('row', row)
-  console.log('value: ', value)
-  if(value.toString() === ''){
-    console.log('error, empty cell')
-    // TODO: catch error
+  let value = ''
+  
+  // Known positions of bricks
+  if (x === 0 || y === 0) {
+    value = 'brick'
+  } else {
+    const cell = rows[y][x]
+    value = cell.cellValue;
+  } 
+  // Checks if Ratman steps on a cell with cheese, or if an empty cell 
+  // Empty cell indicates that Ratman is standing on it
+  if(value === 'cheese' || value === ''){
+    value = 'cheese'
+    eatCheese(x,y)
   }
-  if(value.toString() === 'cheese'){
-    eatCheese()
-  } else if(value.toString() === 'brick'){
+  else if(value === 'brick'){
     collision()
+  } else {
+    value = 'error';
+    console.log('error')
   }
   return value;
  }
@@ -135,97 +118,128 @@ function findIndex(rows, x, y){
  /**
   * Increments point counter and removes cheese from board  
   */
-  function eatCheese(){
+  function eatCheese(x,y){
     // TODO: increment points and remove cheese from cell
-    console.log('eatCheese()')
+    incrementPoints()
+    setHiddenCheese({x:x, y:y, hidden:true});
   }
-
   /**
    * Decrement lives of player and animation?
    */
   function collision(){
     // TODO: collision, decrement lives? 
-    console.log('collision()')
+    decrementPoints();
   }
 
+  /**
+   * Increments point counter by one 
+   * @returns the incremented point counter
+   */
+  function incrementPoints(){
+    let p = 0;
+    p = points + 1;
+    console.log('Increments score')
+    return(setPoints(p));
+  }
+  
+  /**
+   * Decrements point counter by one
+   * @returns The decremented point counter
+   */
+  function decrementPoints(){
+    let p = 0;
+    p = points - 1;
+    console.log('Decrements score')
+    return(setPoints(p));
+  }
+  
+
   // Determines element of each tile 
-  const determineElements = (x, y) => {
+  const determineElements = (hiddenCheese,x, y) => {
     let val = 'brick';
+    
     if (x === 0 || y === 0 || x === width - 1 || y === height - 1) {
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (y === 2 && x < width - 5 && x > 1) {
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (y > 2 && y < height - 2 && x === 2){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (x === 4 && y < height - 2 && y > height - 8){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (x === 5 && y < height - 5 && y > height - 8){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (x === 6 && ((y < height - 5 && y > height - 8) || (y < height - 2 && y > height - 5))){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (x === 7 && y < height - 2 && y > height - 5){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (x === 8 && ((y < height - 2 && y > height - 6) || (y === height - 7))){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
     }
     if (x === 10 && y < height - 2 && y > height - 9){
-      updateOccupiedCell(rows, x, y, val)
+      updateCellValue(rows, x, y, val)
       return <Brick/>
-    }
+    } 
     if (x === position.x && y === position.y) {
-      console.log('RAT position', position)
       return <Rat position={position} />;
     }
-    val = 'cheese'
-    updateOccupiedCell(rows, x, y, val)
-    console.log('rows',rows)
-    checkRatManPosition(rows, x, y)
-    return <Cheese/>
+    // If the cheese has been eaten the cell should be empty 
+    // TODO: Currently only hiding the previously eaten cheese
+    if(hiddenCheese.hidden === true && x === hiddenCheese.x && y === hiddenCheese.y){
+      return;
+    }
+      val = 'cheese'
+      updateCellValue(rows, x, y, val)
+      return <Cheese/>
+
   }
 
   return (
     <>
-    <div className='body'>
+    <div className='body' style={{flexDirection:'row', fontSize:30}}>
       <form onSubmit={navTutorial}>
+        {'Score: '}{points}
         <button
           style={{
             backgroundColor:'black',
             color:'white',
-            fontSize:22,
-            width:40,
-            height:60, 
-            margin:10, 
+            fontSize:20,
+            width:200,
+            height:50, 
             borderRadius:15, 
-            justifyContent:'flex-end',
+            margin:10,
+            marginLeft:400,
             fontWeight:'bold'
           }}
           type='submit'>
-            ?
+            Tutorial
           </button>
         </form>
-
+        </div>
+        <div>
+      
     </div>
-    <div className="game-board" style={{backgroundColor: 'gray',}}>
+    <div className="game-board" style={{backgroundColor: 'gray' }}>
       {rows.map((cells, y) => (
-        <div key={y} className="row">
+        <div key={y} className="row" style={{ }}>
           {cells.map(({ x, y }) => (
-            <div key={`${x}-${y}`} className="cell">
-              {determineElements(x,y)}
+            <div key={`${x}-${y}`} className="cell" style={{ color: 'gray'}}>
+              {determineElements(hiddenCheese,x,y)}
+              
             </div>
           ))}
         </div>
