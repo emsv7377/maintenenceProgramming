13276bb742a1a53b9b70263c1cbaabc3c60aa591
mnordness <michaela.nordness.6056@student.uu.se>
2023-05-11 10:15:55 +0200

    Added translation components

---
+++ src/components/Language.jsx
@@ -0,0 +1,19 @@
+export const ENG = {
+    language: 'English',
+    buttonComponent:{
+        buttonText: 'Start game'
+    }, 
+    headerComponent:{
+        titleText:'Welcome to RatMan'
+    }
+}
+
+export const SWE = {
+    language: 'Svenska',
+    buttonComponent:{
+        buttonText:'Starta spelet'
+    },
+    headerComponent:{
+        titleText:'Välkommen till RatMan'
+    }
+}
--- src/screens/StartScreen.jsx
+++ src/screens/StartScreen.jsx
@@ -1,41 +1,45 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useNavigate } from 'react-router-dom'
-import GameBoard from '../components/GameBoard'
-import pacman from '../assets/pacman.png'
-import index from '../index.css?inline';
+//import GameBoard from '../components/GameBoard'
+//import pacman from '../assets/pacman.png'
+//import index from '../index.css?inline';
 import Rat from '../components/Rat';
 import Cheese from '../components/Cheese';
 
-function StartScreen() {
-   const navigate = useNavigate();
+import LanguageContext from '../components/LanguageContext'
 
-   function navGameBoard(event){
+function StartScreen(){
+    const language = useContext(LanguageContext);
+    const navigate = useNavigate();
+
+    // Navigates to Gameboard 
+    function navGameBoard(event){
         event.preventDefault();
         navigate('GameBoard')
    }
-
+   // Navigates to the tutorial page 
    function navTutorial(event){
-        event.preventDefault();
-        navigate('Tutorial')
+    event.preventDefault();
+    navigate('Tutorial')
     }
-
+    // Navigates to settings page 
     function navSettings(event){
         event.preventDefault();
         navigate('Settings')
     }
 
-   return(
+    return(
         <div className='body'>
-            <h1>Welcome to Rat-Man</h1>
-            <div className='row'
-                style={{height:'20%',width:'20%'}}>
+            <h1>{language.headerComponent.titleText}</h1>
+                <div className='row'
+                    style={{height:'20%',width:'20%'}}>
                         <Rat open={close}/><Cheese/>
-                    </div>
-                    <form onSubmit={navGameBoard}>
-                        <button 
+                </div>
+                <form onSubmit={navGameBoard}>
+                    <button 
                         style={styles}
-                            type='submit'>
-                                Start new game
+                        type='submit'>
+                            {language.buttonComponent.buttonText}
                         </button>
                     </form>
                     <form onSubmit={navTutorial}>
